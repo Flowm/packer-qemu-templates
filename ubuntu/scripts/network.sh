3fb9104c632849c7ce53b26d18696d1ef42097af
Florian Mauracher <florian.mauracher@gmx.net>
2016-11-06 11:32:14 +0100

    Add predictable network if names and dist upgrade to preseed for xenial

--- ubuntu/scripts/network.sh
+++ ubuntu/scripts/network.sh
@@ -14,7 +14,7 @@ iface eth0 inet dhcp
 # This section will be auto-replaced
 auto eth1
 iface eth1 inet manual
-pre-up sleep 2
+  pre-up sleep 2
 
 #VAGRANT-END
 EOF
@@ -22,6 +22,3 @@ EOF
 # Disable DNS reverse lookup
 sed -i "/[# ]*UseDNS[ \t].*/d" /etc/ssh/sshd_config
 echo "UseDNS no" >> /etc/ssh/sshd_config
-
-sed -i 's/[ #]*GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-update-grub
